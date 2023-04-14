6d51ea69c70b0f4a835fd6ec552b2d8f93057e33
easydevgit <urserg1@gmail.com>
2023-04-14 17:29:38 +0400

    feat(FileManager): add alexusmai/laravel-file-manager and configure it

--- resources/views/admin/file_manager.blade.php
+++ resources/views/admin/file_manager.blade.php
@@ -1,8 +1,12 @@
 @extends(backpack_view('blank'))
 
 @section('content')
+    <link rel="stylesheet" href="{{ asset('vendor/file-manager/css/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/file-manager/css/all.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/file-manager/css/file-manager.css') }}">
 <div class="jumbotron">
     <h1 class="mb-4">{{ __('models.file_manager') }}</h1>
-
+    <div id="fm" style="height: 600px;"></div>
+    <script src="{{ asset('vendor/file-manager/js/file-manager.js') }}"></script>
 </div>
 @endsection
