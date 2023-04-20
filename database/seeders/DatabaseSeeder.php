65fdc20d1a79fbada8adc123eeae6de3a80e8d38
easydevgit <urserg1@gmail.com>
2023-04-20 17:31:23 +0400

    fix(Seeders): add LoggingSettingsSeeder to DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
 //        $this->call(AuthorsSeeder::class);
         $this->call([RoleSeeder::class]);
         $this->call(AdminSeeder::class);
+        $this->call(LoggingSettingsSeeder::class);
     }
 }
