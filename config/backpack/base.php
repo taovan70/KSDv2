339b9fb4fc1911daa6cadc9b89702d7bad4c41a9
easydevgit <urserg1@gmail.com>
2023-05-05 11:46:29 +0400

    feature(performance): add instant.page script

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -135,6 +135,7 @@ return [
     'scripts' => [
         // Backstrap includes jQuery, Bootstrap, CoreUI, PNotify, Popper
         'packages/backpack/base/js/bundle.js',
+        'js/instant-page.js'
 
         // examples (everything inside the bundle, loaded from CDN)
         // 'https://code.jquery.com/jquery-3.4.1.min.js',
