9500c6a17dc6f81d21b49c74a55c75354d18b60b
easydevgit <urserg1@gmail.com>
2023-06-19 23:44:14 +0400

    fix(app): spatie folders permission

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,6 +40,16 @@ return [
             'driver' => 'local',
             'root'   => public_path('storage/media'),
             'url'    => env('APP_URL').'/storage/media',
+            'permissions' => [
+                'file' => [
+                    'public' => 0774,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0775,
+                    'private' => 0700,
+                ],
+            ],
         ],
 
         'public' => [
