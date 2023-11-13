b1241209c7f31d6d0f6d820785e26d55c19dc1ee
easydevgit <urserg1@gmail.com>
2023-11-13 19:36:49 +0400

    feat(Make Article): hot show preview button on new article

--- resources/js/Pages/MakeArticle.vue
+++ resources/js/Pages/MakeArticle.vue
@@ -277,7 +277,7 @@ const copyToClipboard = (val) => {
           <div v-if="endpointForm.errors.mainPic" class="form_error_text">{{ endpointForm.errors.mainPic }}</div>
         </div>
       </div>
-      <el-button @click="sendForm(false)" color="#626aef">{{ $t("makeArticle.fields.save") }}</el-button>
+      <el-button v-if="props.article?.id" @click="sendForm(false)" color="#626aef">{{ $t("makeArticle.fields.save") }}</el-button>
       <el-button @click="sendForm(true)" color="#626aef">{{ $t("makeArticle.fields.preview") }}</el-button>
     </form>
 
