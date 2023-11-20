104e02d0a8b2ce8bf0c83468a712319f2b3a5356
easydevgit <urserg1@gmail.com>
2023-11-20 10:58:34 +0400

    refactor(Sub Cat): know_more_about_each block articles slug

--- app/Http/Resources/api/Blocks/SubCategory/SubCatKnowMoreAboutEachBlockResource.php
+++ app/Http/Resources/api/Blocks/SubCategory/SubCatKnowMoreAboutEachBlockResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\api\Blocks\SubCategory;
 
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,11 +16,23 @@ class SubCatKnowMoreAboutEachBlockResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $blockDataFromDB = json_decode($this->block_data) ?? [];
+
+        $data = [];
+
+        foreach ($blockDataFromDB as $value) {
+            $data[] = [
+                'name' => $value->name ?? '',
+                'photo_path' => $value->photo_path ?? '',
+                'article_one_slug' => Article::select('slug')->where('id', $value->article_one_id)?->first()?->slug,
+                'article_two_slug' => Article::select('slug')->where('id', $value->article_two_id)?->first()?->slug,
+            ];
+        }
 
         return [
             'name' => $this->name,
             'category_id' => $this->category_id,
-            'block_data' => json_decode($this->block_data) ?? [],
+            'block_data' => $data,
         ];
     }
 }
