cef48c34fff92da488f4abd26e8f701b6c0c2f58
easydevgit <urserg1@gmail.com>
2023-11-22 13:32:19 +0400

    feat(Blocks): articles link on site

--- app/Http/Controllers/Admin/BigCardArticleCrudController.php
+++ app/Http/Controllers/Admin/BigCardArticleCrudController.php
@@ -47,13 +47,14 @@ class BigCardArticleCrudController extends CrudController
         CRUD::column('name')->label(__('table.name'));
         CRUD::addColumn([
             'label' => __('table.article'),
-            'type' => 'select',
             'name' => 'article_id',
             'attribute' => 'name',
             'entity' => 'article',
             'wrapper' => [
-                'href' => fn ($crud, $column, $article, $article_id) => backpack_url("article/{$article_id}/show")
-            ]
+                'href' => 'javascript:void(0);',
+                'onclick' => 'setPreviewCookie();window.open(this.dataset.url,`_blank`);return false;',
+                'data-url' => fn($crud, $column, $article, $category_id) => (env('FRONT_URL')."/article-preview-".$article['id']),
+            ],
         ]);
 
         Widget::add([
@@ -121,13 +122,14 @@ class BigCardArticleCrudController extends CrudController
         CRUD::column('name')->label(__('table.name'));
         CRUD::addColumn([
             'label' => __('table.article'),
-            'type' => 'select',
             'name' => 'article_id',
             'attribute' => 'name',
             'entity' => 'article',
             'wrapper' => [
-                'href' => fn ($crud, $column, $article, $article_id) => backpack_url("article/{$article_id}/show")
-            ]
+                'href' => 'javascript:void(0);',
+                'onclick' => 'setPreviewCookie();window.open(this.dataset.url,`_blank`);return false;',
+                'data-url' => fn($crud, $column, $article, $category_id) => (env('FRONT_URL')."/article-preview-".$article['id']),
+            ],
         ]);
         CRUD::addColumn([
             'name' => 'photo_path',
