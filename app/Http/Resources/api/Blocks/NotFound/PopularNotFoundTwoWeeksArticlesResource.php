c8610bc5c7776694c5305349d342e9ccecc3bbc2
easydevgit <urserg1@gmail.com>
2023-12-15 10:51:21 +0400

    refactor(Not Found): popular two weeks articles block api route alter resource

--- app/Http/Resources/api/Blocks/NotFound/PopularNotFoundTwoWeeksArticlesResource.php
+++ app/Http/Resources/api/Blocks/NotFound/PopularNotFoundTwoWeeksArticlesResource.php
@@ -15,28 +15,27 @@ class PopularNotFoundTwoWeeksArticlesResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $mainPic = !empty($this->article) ?  Article::where('slug', '=',  $this->article->slug)->with('media')->first()?->getMedia('mainPic') : '';
+        $mainPic = !empty($this->article) ? Article::where('slug', '=', $this->article->slug)->with('media')->first(
+        )?->getMedia('mainPic') : '';
         return [
-            'name' => $this->name,
-            'article' => [
-                'name' => $this->article?->name,
-                'slug' => $this->article?->slug,
-                'publish_date' => $this->article?->publish_date,
-                'author' => [
-                    'id' => $this->article?->author->id,
-                    'name' => $this->article?->author->name,
-                    'surname' => $this->article?->author->surname,
-                    'slug' => $this->article?->author->slug,
-                ],
-                'category' => [
-                    'name' => $this->article?->category->name,
-                    'slug' => $this->article?->category->slug,
-                    'icon_path' => $this->article?->category->icon_path
-                ],
-                'media' => [
-                    'mainPic' => $mainPic,
-                ]
+            'name' => $this->article?->name,
+            'slug' => $this->article?->slug,
+            'publish_date' => $this->article?->publish_date,
+            'author' => [
+                'id' => $this->article?->author->id,
+                'name' => $this->article?->author->name,
+                'surname' => $this->article?->author->surname,
+                'slug' => $this->article?->author->slug,
             ],
+            'category' => [
+                'name' => $this->article?->category->name,
+                'slug' => $this->article?->category->slug,
+                'icon_path' => $this->article?->category->icon_path
+            ],
+            'media' => [
+                'mainPic' => $mainPic,
+            ]
+
         ];
     }
 }
