f11583ee50c4c13aafa24ab54b7be7226e0e3589
easydevgit <urserg1@gmail.com>
2023-12-15 10:11:19 +0400

    feat(Not Found): popular two weeks articles block api route add pagination

--- app/Http/Controllers/Api/Blocks/NotFound/PopularNotFoundTwoWeeksArticlesController.php
+++ app/Http/Controllers/Api/Blocks/NotFound/PopularNotFoundTwoWeeksArticlesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Blocks\NotFound;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\api\Blocks\NotFound\PopularNotFoundTwoWeeksArticlesResource;
 use App\Models\Blocks\NotFound\PopularNotFoundTwoWeeksArticles;
+use Illuminate\Http\Request;
 
 
 class PopularNotFoundTwoWeeksArticlesController extends Controller
@@ -14,13 +15,13 @@ class PopularNotFoundTwoWeeksArticlesController extends Controller
     {
     }
 
-    public function index(): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+    public function index(Request $request): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
     {
         $result = PopularNotFoundTwoWeeksArticles::with(['article' => function ($query) {
             $query->where('published', 1);
             $query->with('author');
             $query->with('category');
-        }])->get();
+        }])->paginate($request->items_per_page ?? 6);
 
         return PopularNotFoundTwoWeeksArticlesResource::collection($result);
     }
